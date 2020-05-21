82329c82a6b5f0a79f33ec83418954e1d536f521
Hidden He <he15810251026@gmail.com>
2020-05-21 09:42:28 -0700

    updates
    venus run

--- model_config.py
+++ model_config.py
@@ -18,7 +18,7 @@ transmitter_output_act_fn = None
 transmitter_output_dim = encoder_latent_dimension
 #learning configurations
 kernel_regularizer_l = 0.0001
-pre_training_lr = 5e-3 #fixed, may need to loose regularization for pre-training
+pre_training_lr = 1e-3 #fixed, may need to loose regularization for pre-training
 fine_tuning_lr = 1e-3 #too small learning rates may leads to over-fitting like behavior
 decay = 0.8
 max_epoch = 200
