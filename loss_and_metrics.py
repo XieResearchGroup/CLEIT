696448dcbf43d6aece2cfc137253686ad93418dd
Hidden He <he15810251026@gmail.com>
2021-02-04 17:50:24 -0800

    Update loss_and_metrics.py

--- loss_and_metrics.py
+++ loss_and_metrics.py
@@ -140,7 +140,7 @@ def compute_cosine_distances_matrix(x, y):
 def contrastive_loss(y_true, y_pred, device):
     sim_matrix = compute_cosine_distances_matrix(y_true, y_pred)
     denominator = torch.sum(torch.mul(torch.exp(sim_matrix), -1 * (
-            torch.eye(n=sim_matrix.shape[0], dtype=torch.float32) - 1)).to(device), dim=0) + torch.sum(torch.mul(torch.exp(sim_matrix), -1 * (
+            torch.eye(n=sim_matrix.shape[0], dtype=torch.float32).to(device) - 1)), dim=0) + torch.sum(torch.mul(torch.exp(sim_matrix), -1 * (
                           torch.eye(n=sim_matrix.shape[0], dtype=torch.float32).to(device) - 1)), dim=1)
     nominator = torch.sum(torch.mul(torch.exp(sim_matrix), torch.eye(n=sim_matrix.shape[0], dtype=torch.float32).to(device)), dim=0)
 
